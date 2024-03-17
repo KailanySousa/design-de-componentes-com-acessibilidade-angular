bb0d42d3913b4458e70b33af330b64a20c4c69b3
KailanySousa <kailany-sousa2010@hotmail.com>
2024-03-17 13:05:21 -0300

    feat: implementando interface ControlValueAccessor

--- src/app/shared/components/yes-no-button-group/yes-no-button-group.component.ts
+++ src/app/shared/components/yes-no-button-group/yes-no-button-group.component.ts
@@ -1,20 +1,41 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { ControlValueAccessor } from '@angular/forms';
 
 @Component({
   selector: 'app-yes-no-button-group',
   templateUrl: './yes-no-button-group.component.html',
   styleUrls: ['./yes-no-button-group.component.scss']
 })
-export class YesNoButtonGroupComponent {
+export class YesNoButtonGroupComponent implements ControlValueAccessor {
   @Input() public value: string = null;
   @Input() public label: string = '';
   @Output() public valueChange = new EventEmitter<string>();
 
   public options = YesNoButtonGroupOptions;
 
+  public onChange = (value: string) => { };
+  public onTouched = (value: string) => { };
+
   public activate(value: string) {
     this.value = value;
     this.valueChange.emit(this.value);
+  }
+
+  public writeValue(value: string): void {
+    this.value = value;
+    this.onChange(this.value);
+  }
+
+  public registerOnChange(fn: (value: string) => void): void {
+    this.onChange = fn;
+  }
+
+  public registerOnTouched(fn: (value: string) => void): void {
+    this.onTouched = fn
+  }
+
+  public setDisabledState?(isDisabled: boolean): void {
+    throw new Error('Method not implemented.');
   }
 }
 
