9b469ac9474357cea6ac9f829badbd505d90f1e2
KailanySousa <kailany-sousa2010@hotmail.com>
2024-03-17 16:25:55 -0300

    feat: implementando NG_VALUE_ACCESSOR

--- src/app/shared/components/yes-no-button-group/yes-no-button-group.component.ts
+++ src/app/shared/components/yes-no-button-group/yes-no-button-group.component.ts
@@ -1,10 +1,15 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
-import { ControlValueAccessor } from '@angular/forms';
+import { Component, EventEmitter, Input, Output, forwardRef } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 
 @Component({
   selector: 'app-yes-no-button-group',
   templateUrl: './yes-no-button-group.component.html',
-  styleUrls: ['./yes-no-button-group.component.scss']
+  styleUrls: ['./yes-no-button-group.component.scss'],
+  providers: [{
+    provide: NG_VALUE_ACCESSOR,
+    multi: true,
+    useExisting: forwardRef(() => YesNoButtonGroupComponent)
+  }]
 })
 export class YesNoButtonGroupComponent implements ControlValueAccessor {
   @Input() public value: string = null;
@@ -17,13 +22,13 @@ export class YesNoButtonGroupComponent implements ControlValueAccessor {
   public onTouched = (value: string) => { };
 
   public activate(value: string) {
-    this.value = value;
-    this.valueChange.emit(this.value);
+    this.writeValue(value);
   }
 
   public writeValue(value: string): void {
     this.value = value;
     this.onChange(this.value);
+    this.valueChange.emit(this.value);
   }
 
   public registerOnChange(fn: (value: string) => void): void {
