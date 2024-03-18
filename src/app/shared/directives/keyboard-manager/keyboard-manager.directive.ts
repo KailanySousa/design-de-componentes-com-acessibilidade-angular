ffb13e780a6195d4a9b34f1696e3b17eedafcbbe
KailanySousa <kailany-sousa2010@hotmail.com>
2024-03-18 11:54:36 -0300

    feat: lógica do teclado

--- src/app/shared/directives/keyboard-manager/keyboard-manager.directive.ts
+++ src/app/shared/directives/keyboard-manager/keyboard-manager.directive.ts
@@ -1,27 +1,48 @@
-import { Directive, HostListener } from "@angular/core";
+import { ContentChildren, Directive, HostListener, QueryList } from "@angular/core";
+import { KeyboardManagedItemDirective } from "./keyboard-managed-item/keyboard-managed-item.directive";
 
 @Directive({
     selector: '[appKm]'
 })
 export class KeyboardManagerDirective { 
 
+    @ContentChildren(KeyboardManagedItemDirective)
+    public items: QueryList<KeyboardManagedItemDirective> = null;
+        
     @HostListener('keyup', ['$event'])
     public managetKeys(event: KeyboardEvent): void {
         switch (event.key) {
             case 'ArrowUp':
-                console.log('up');
+                this.moveFocus(ArrowDirection.RIGHT).focus();
                 break;
             case 'ArrowDown':
-                console.log('down');
+                this.moveFocus(ArrowDirection.LEFT).focus();
                 break;
-             case 'ArrowLeft':
-                console.log('left');
+            case 'ArrowLeft':
+                this.moveFocus(ArrowDirection.LEFT).focus();
                 break;
             case 'ArrowRight':
-                console.log('right');
+                this.moveFocus(ArrowDirection.RIGHT).focus();
                 break;
             default:
                 break;
         }
     }
+
+    public moveFocus(direction: ArrowDirection): KeyboardManagedItemDirective {
+        const items = this.items.toArray();
+        const currentSelectedIndex = items.findIndex(item => item.isFocused());
+        const targetElementFocus = items[currentSelectedIndex + direction];
+
+        if (targetElementFocus) {
+            return targetElementFocus
+        }
+
+        return direction === ArrowDirection.LEFT ? items[items.length - 1] : items[0];
+    }
+}
+
+enum ArrowDirection {
+    LEFT = -1,
+    RIGHT = 1
 }
