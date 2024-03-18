a31e0eb91da442ea852e4bc5c9a46d92272f27cb
KailanySousa <kailany-sousa2010@hotmail.com>
2024-03-18 09:22:52 -0300

    feat: criando a diretiva KeyboardManager

---
+++ src/app/shared/directives/keyboard-manager/keyboard-manager.directive.ts
@@ -0,0 +1,27 @@
+import { Directive, HostListener } from "@angular/core";
+
+@Directive({
+    selector: '[appKm]'
+})
+export class KeyboardManagerDirective { 
+
+    @HostListener('keyup', ['$event'])
+    public managetKeys(event: KeyboardEvent): void {
+        switch (event.key) {
+            case 'ArrowUp':
+                console.log('up');
+                break;
+            case 'ArrowDown':
+                console.log('down');
+                break;
+             case 'ArrowLeft':
+                console.log('left');
+                break;
+            case 'ArrowRight':
+                console.log('right');
+                break;
+            default:
+                break;
+        }
+    }
+}
